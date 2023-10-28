01401da1816b2f83239e0e5dc30c9e6f355dd1f2
Fabian Hummel <hum22501@spengergasse.at>
2023-10-28 21:04:18 +0200

    Added express, socket.io and uWebSockets

--- index.ts
+++ index.ts
@@ -1,16 +1,44 @@
 
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from "socket.io";
+import { App as MicroWebSockets } from "uWebSockets.js";
 
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  transports: ["websocket", "polling"],
+  cors: {
+    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "http://localhost:5173/", // TODO: change to harmonyhub.com
+    credentials: true,
+  }
+});
+
+const uws = MicroWebSockets();
+io.attachApp(uws);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  
+  // send a message to the client
+  socket.broadcast.emit("hello", "world");
+
+  // receive a message from the client 
+  socket.on("howdy", (arg) => {
+    console.log(arg); // prints "stranger" 
+  });
+});
+
+const port = process.env.PORT;
+server.listen(port, () => {
   console.log(`Server is 🔥 at http://localhost:${port}`);
 });
